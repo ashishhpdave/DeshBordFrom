66f66c6e6a4f6c4564a38ed6ba6475cef955cf3e
ashishhpdave <141114347+ashishhpdave@users.noreply.github.com>
2023-12-04 12:24:01 +0530

    mongourlChange

--- module.js/index.js
+++ module.js/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const url = 'mongodb://0.0.0.0:27017/deshbordFrom'
+const url = 'mongodb+srv://sutharpankaj723:pankaj123123@pankaj.dlszltz.mongodb.net/furniture'
 
 function connectDB() {
     try {
