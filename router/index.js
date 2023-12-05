ff524601e81a8b0c5d7c33e173bf1fac595e9e62
ashishhpdave <141114347+ashishhpdave@users.noreply.github.com>
2023-12-05 11:11:24 +0530

    Update index.js

--- router/index.js
+++ router/index.js
@@ -3,8 +3,11 @@ const express = require('express')
 const signup = require('./controller/signup')
 const login = require('./controller/login')
 const router = express.Router()
+router.get('/', (req, res) => {
+    res.send('hello')
 
+})
 router.post('/signup', signup)
-router.post('/login',login)
+router.post('/login', login)
 
 module.exports = router
