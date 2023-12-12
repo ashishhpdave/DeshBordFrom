34901dd62aa8f9094d6fccabb6f46090759c8d2d
ashishhpdave <141114347+ashishhpdave@users.noreply.github.com>
2023-12-12 23:17:36 +0530

    billingUpdateData

---
+++ router/controller/Billing/update.js
@@ -0,0 +1,21 @@
+const { BilingModel } = require("../../../module.js");
+
+async function BillingUpdateData(req,res){
+    try {
+        const isUpdate = await BilingModel.updateOne({email:req.body.email},{f_name:req.body.f_name,l_name:req.body.l_name,email:req.body.email,message:req.body.message,Country:req.body.Country,Company_Name:req.body.Company_Name,Address:req.body.Address,Apartment:req.body.Apartment,State_Country:req.body.State_Country,Posta:req.body.Posta,Email_Address:req.body.Email_Address,Phone:req.body.Phone})
+        if(!isUpdate){
+            throw new Error("Data was not updated.....")
+        }
+        res.status(200).json({
+             msg : "Data successfully Updated....",
+             status : true
+        })
+    } catch (error) {
+        res.status(409).json({
+            status : false,
+            message : error.message
+        })
+    }
+}
+
+module.exports = BillingUpdateData
--- router/index.js
+++ router/index.js
@@ -8,6 +8,7 @@ const deleteData = require('./controller/contact/delete')
 const updateData = require('./controller/contact/update')
 const BillingData = require('./controller/Billing/billing')
 const BillingDataDelete = require('./controller/Billing/delete')
+const BillingUpdateData = require('./controller/Billing/update')
 
 const router = express.Router()
 router.get('/', (req, res) => {
@@ -21,6 +22,7 @@ router.get('/subscribe',subscribeData)
 router.get('/contact',ContactData)
 router.get('/billing',BillingData)
 router.get('/billingdeleteData',BillingDataDelete)
+router.get('/billingupdateData',BillingUpdateData)
 router.get('/delete',deleteData)
 router.get('/update',updateData)
 
